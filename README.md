859f6162b1fa367995786dd2ec5ff9eb20ede15e
Christian <67313472+christianbauer1@users.noreply.github.com>
2021-09-10 08:21:28 +0000

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Blackjack
+A simple console game of Blackjack, played by the following rules:
+- The deck is unlimited in size. 
+- There are no jokers. 
+- The Jack/Queen/King all count as 10.
+- The the Ace can count as 11 or 1.
+- The cards in the list have equal probability of being drawn.
+- Cards are not removed from the deck as they are drawn.
--- main.py
+++ main.py
@@ -1,12 +1,3 @@
-############### Blackjack Rules #####################
-
-## The deck is unlimited in size. 
-## There are no jokers. 
-## The Jack/Queen/King all count as 10.
-## The the Ace can count as 11 or 1.
-## The cards in the list have equal probability of being drawn.
-## Cards are not removed from the deck as they are drawn.
-
 import random
 from replit import clear
 from art import logo
